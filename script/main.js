e40ef579416c05991a00b655f00825fcca51d1b9
Jgschwind <141969207+Jgschwind@users.noreply.github.com>
2023-08-25 23:41:39 -0300

    Arreglos + nueva seccion transf.
    Arreglo de error de funcionamiento de prestamos y pre-diseños de transferencias.

--- script/main.js
+++ script/main.js
@@ -1,11 +1,10 @@
-
 //FUNCION MENU ACORDEON
-var acc = document.getElementsByClassName("accordion");
-var i;
+let acc = document.getElementsByClassName("accordion");
+let i;
 for (i = 0; i < acc.length; i++) {
     acc[i].addEventListener("click", function () {
         this.classList.toggle("active");
-        var panel = this.nextElementSibling;
+        let panel = this.nextElementSibling;
         if (panel.style.maxHeight) {
             panel.style.maxHeight = null;
         } else {
@@ -39,45 +38,4 @@ function showSlides(n) {
     dots[slideIndex - 1].className += " active";
 }
 
-// calculo de intereses
 
-const cacl = document.querySelector(".calcular")
-
-function calcularPorcentaje (a, b){
-    var interes;
-        if(a <= 3)i = 0.025; // 2.5%
-        else if(a > 3 && a < 6) interes = 0.05; // 5% 
-        else if(a >= 6 && a < 10) interes = 0.075 // 7.5% 
-        else if (a >= 10 && a < 14) interes = 0.1 // 10%
-
-    let operacion =  b * a * interes;
-    return operacion
-    }
-
-function calcularTotal(a, b) {
-    let number1 = parseInt(a);
-    let number2 = parseInt(b);
-    return number1 + number2
-} 
-
-function calcularCuota(a, b) {
-   console.log(a/b)
-    return a/b
-}
-
-cacl.addEventListener("click",()=>{
-    const monto = document.querySelector(".amount").value;
-    const meses = document.querySelector(".month").value;
-    const tax = document.querySelector(".tax")
-    const amountResult = document.querySelector(".amount-result")
-    const monthTax = document.querySelector(".month-tax")
-
-    let taxResult = calcularPorcentaje(meses, monto); 
-    let final = calcularTotal(monto, taxResult);
-    let taxMounth = calcularCuota(final, meses)
-
-    amountResult.textContent = `Monto a devolver: $${final}`
-    tax.textContent =`intereses: $${taxResult}`;
-    monthTax.textContent =`Cuota mensual: $${taxMounth}`;
-}
-)

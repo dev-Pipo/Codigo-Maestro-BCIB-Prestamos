7de7f9d98f6ba6c2049257735f48b69d4cfd7a34
dev-Pipo <lompardiab2@gmail.com>
2023-08-27 22:20:18 -0300

    update css

--- script/transfer.js
+++ script/transfer.js
@@ -6,24 +6,26 @@ var montoDeSaldoBancario = 100000; // saldo de prueba
 let pagarServicios = document.querySelector(".pagar")
 
 
-saldoActual.textContent= `$${montoDeSaldoBancario}`
+saldoActual.textContent= `$${montoDeSaldoBancario}`;
 
 transferencia.addEventListener("click",()=>{ 
+    let restoBancario = document.querySelector(".transfer-amount").value;
+    if(montoDeSaldoBancario < restoBancario) return alert("saldo insuficiente")
     let validar = confirm("seguro que quieres realizar esta trasferencia")
     if(validar == true){ 
-    let restoBancario = document.querySelector(".transfer-amount").value;
     montoDeSaldoBancario -= restoBancario;
     saldoActual.textContent=`$${montoDeSaldoBancario}`;
     alert("su operacion a sido realizada con exito")}
     else alert("Su operacion a sido cancelada")}
     )
 
-pagarServicios.addEventListener("click",()=>{ 
-     let validar = confirm("seguro que quieres realizar esta trasferencia")
-    if(validar == true){ 
-    let restoBancario = document.querySelector(".transfer-amount").value;
-    montoDeSaldoBancario -= restoBancario;
-    saldoActual.textContent=`$${montoDeSaldoBancario}`;
-    alert("su operacion a sido realizada con exito")}
-    else alert("Su operacion a sido cancelada")}
-    )
+   pagarServicios.addEventListener("click",()=>{ 
+        let restoBancario = document.querySelector(".pay-amount").value;
+        if(montoDeSaldoBancario < restoBancario) return alert("saldo insuficiente")
+        let validar = confirm("seguro que quieres realizar esta trasferencia")
+        if(validar == true){ 
+        montoDeSaldoBancario -= restoBancario;
+        saldoActual.textContent=`$${montoDeSaldoBancario}`;
+        alert("su operacion a sido realizada con exito")}
+        else alert("Su operacion a sido cancelada")}
+        )

6a2c1e32e61e72ce492b74f12ec54fbab7f7b853
Jgschwind <141969207+Jgschwind@users.noreply.github.com>
2023-08-27 21:48:32 -0300

    Agregado validacion login

--- script/signup.js
+++ script/signup.js
@@ -1,15 +1,38 @@
 //VALIDACION INICIO DE SESION
-const username = document.getElementById('username')
-const password = document.getElementById('password')
-const button = document.getElementById('button-login1')
+/Obtener los elementos del formulario./
+var formulario = document.getElementById("formulario");
+var usuario = document.getElementById("username");
+var contrasena = document.getElementById("password");
 
-button.addEventListener('click'), (e) => {
-    e.preventDefault()
-    const data = {
-        username: username.value ,
-        password: password.value
-    }
+// Añadir un evento de envío al formulario
+formulario.addEventListener("submit", function(evento) {
+  // Prevenir el comportamiento por defecto del formulario
+  evento.preventDefault();
+  // Validar los campos de usuario y contraseña
+  if (usuario.value === "") {
+    // Mostrar un mensaje de alerta si el campo usuario está vacío
+    alert("Por favor, ingrese su nombre de usuario.");
+    // Enfocar el campo usuario
+    usuario.focus();
+    // Retornar falso para cancelar el envío del formulario
+    return false;
+  }
+  if (contrasena.value === "") {
+    // Mostrar un mensaje de alerta si el campo contraseña está vacío
+    alert("Por favor, ingrese su contraseña.");
+    // Enfocar el campo contraseña
+    contrasena.focus();
+    // Retornar falso para cancelar el envío del formulario
+    return false;
+  }
+  // Comprobar si el usuario es maxi y la contraseña es maxi1234
+  if (usuario.value !== "maxi" || contrasena.value !== "maxi1234") {
+    // Mostrar un mensaje de alerta si el usuario o la contraseña no son correctos
+    alert("Usuario o contraseña incorrectos.");
+    // Retornar falso para cancelar el envío del formulario
+    return false;
+  }
+  // Si todo está bien, enviar el formulario
+  window.location.href = "./profile.html"
 
-    window.location = 'profile.html';
-    console.log(data)
-}
+});
